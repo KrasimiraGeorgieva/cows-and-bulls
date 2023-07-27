255293dbed73a7b7793df92e95817f81ff633ca3
Krasimira Georgieva <krassi77bg@gmail.com>
2023-07-27 11:05:31 +0300

    Change page title

--- resources/views/game/high_scores.blade.php
+++ resources/views/game/high_scores.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>High Scores</title>
+    <title>Top 10 best results</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
